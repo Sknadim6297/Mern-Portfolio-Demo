7c4cb3f2fa1dd84ee6151d0d32b60945c218809c
SK NADIM <128034110+Sknadim6297@users.noreply.github.com>
2024-08-23 12:09:10 +0530

    Commit

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 //middlewares
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
